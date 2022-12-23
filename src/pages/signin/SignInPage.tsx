00005fdf8f4d23f7c1e0141498e3c9b5ac7f407b
vupham <aflyduck@outlook.com>
2022-12-23 08:31:51 +0700

    [feat - sign in] create sign in page

---
+++ src/pages/signin/SignInPage.tsx
@@ -0,0 +1,12 @@
+import {SignInForm} from "./SignInForm";
+
+function SignInPage()
+{
+   return (
+      <main className={"flex min-h-screen justify-center items-center"}>
+         <SignInForm className={""}/>
+      </main>
+   )
+}
+
+export {SignInPage}
