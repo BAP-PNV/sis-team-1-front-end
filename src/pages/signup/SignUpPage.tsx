63c6466496f891081ff05543fb0e5b680c3cd247
vupham <aflyduck@outlook.com>
2022-12-23 08:32:37 +0700

    [feat - sign up] create sign up page

---
+++ src/pages/signup/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import {SignUpForm} from "./SignUpForm";
+
+function SignUpPage()
+{
+   return (
+      <main className={"flex min-h-screen justify-center items-center"}>
+         <SignUpForm/>
+      </main>
+   )
+}
+
+export {SignUpPage}
