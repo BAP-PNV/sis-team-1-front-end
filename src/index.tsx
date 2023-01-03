6be8fa99e5df4101d7abf7b40a7298b812de6d09
vupham <aflyduck.dev@outlook.com>
2023-01-03 14:42:19 +0700

    [feat] add DashboardRoutes to the app

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 import "./style/index.css"
 
+import {BrowserRouter} from "react-router-dom"
+import {DashboardRoutes} from "./router"
+
 const root = ReactDOM.createRoot(
    document.getElementById('root') as HTMLElement
 )
 
 root.render(
    <React.StrictMode>
-
+      <BrowserRouter>
+         <DashboardRoutes/>
+      </BrowserRouter>
    </React.StrictMode>
 )
 
