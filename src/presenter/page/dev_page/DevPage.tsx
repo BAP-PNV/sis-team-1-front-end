41b7f6230222fc6d7e584578c84adb61aacfd7ad
vupham <aflyduck.dev@outlook.com>
2023-01-12 14:13:50 +0700

    yeah

--- src/presenter/page/dev_page/DevPage.tsx
+++ src/presenter/page/dev_page/DevPage.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {env} from "../../../constant";
 
 function DevPage()
 {
@@ -14,13 +15,22 @@ function DevPage()
 
   return (
     <div className={"p-5"}>
-      <section className={"flex justify-between gap-5"}>
+      <section className={"grid grid-cols-[1.5fr_1fr] justify-between gap-5"}>
         <iframe
           className={"w-full min-h-screen"}
           src="http://localhost:8080"
           name="targetframe"
           allowFullScreen={true}>
         </iframe>
+        <div className={"border-gray-300 border-l p-5"}>
+
+          <h2 className={"text-2xl mb-4"}>Document</h2>
+
+          <div className={"mb-10"}>
+            <p>API URL</p>
+            <p className={"underline text-blue-700"}>{env.apiEndpoint}/<span className={"text-black"}>[your_uri]</span>?access_key=<span className={"text-black"}>[your_secret_key]</span></p>
+          </div>
+        </div>
       </section>
     </div>
   )
